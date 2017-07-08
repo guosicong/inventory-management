e26b198cf77221a8a90a10e95476520018ea4825
guosicong <zgguosicong@gmail.com>
2017-07-08 11:58:11 +0800

    2017年7月8日

--- src/main/java/inventory/springservice/configuration/SecurityConfiguration.java
+++ src/main/java/inventory/springservice/configuration/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package inventory.springservice.configuration;
 
 import inventory.springservice.domain.CustomUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
